ed96ca16b80114f247fd5f30357559ba78cb5f65
Muhammad Tayyab <tayyabdeveloper15@gmail.com>
2024-11-07 09:36:34 +0500

    Add Project

---
+++ src/ContactForm/Form.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import FormInfo from "./FormInfo";
+import FormInput from "./FromInput";
+
+export default function Form() {
+  return (
+    <section>
+      <form action="">
+        <FormInfo />
+        <FormInput />
+      </form>
+    </section>
+  );
+}
---
+++ src/ContactForm/FormInfo.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function FormInfo() {
+  return (
+    <section>
+      {/* Heading  */}
+      <div>
+      <h1></h1>
+      </div>
+      
+
+    </section>
+  )
+}
